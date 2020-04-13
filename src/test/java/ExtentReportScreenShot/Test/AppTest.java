be909c55034649b4ab83f5aa832c08066ecd610d
Allu Varaprasad <ramchandraallu@gmail.com>
2020-04-13 09:08:02 +0530

    Update coed for log4j

--- src/test/java/ExtentReportScreenShot/Test/AppTest.java
+++ src/test/java/ExtentReportScreenShot/Test/AppTest.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -32,8 +33,10 @@ public class AppTest
 	WebDriver driver;
 	ExtentReports extent;
 	ExtentTest extenttest;
+	Logger log = Logger.getLogger(AppTest.class);
 	@BeforeTest
 	public void report() {
+		log.info("Indide Before MEthod");
 		extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/ExtentReport.html",true);
 		extent.addSystemInfo("username", "Allu");
 		extent.addSystemInfo("os","windows");
@@ -42,6 +45,7 @@ public class AppTest
 	
 	@AfterTest
 	public void afterTest() {
+		log.warn("Indide Afetr MEthod");
 		extent.flush();
 		extent.close();
 	}
@@ -50,11 +54,14 @@ public class AppTest
    public void setup() {
 	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lalitha\\Desktop\\PanduAutomation\\chromedriver.exe");
 		driver = new ChromeDriver();
+		log.info("open url");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://classic.freecrm.com/index.html");
+		//driver.get("https://classic.freecrm.com/index.html");
+		driver.get("https://www.facebook.com/");
+		log.error("taking longer time than expected");
    }
    public String screenshot(WebDriver driver,String screenshotname) throws IOException {
 	   String date = new SimpleDateFormat("yyymmddhhmmss").format(new Date());
@@ -67,28 +74,34 @@ public class AppTest
    
    @Test 
    public void verifyTitle() {
+	   log.info("***********************Started***********************************");
 	   extenttest=extent.startTest("verifyTitle");
-	   Assert.assertEquals(driver.getTitle(), "wrongtitle");
-	   
+	   Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
+	   log.info("***********************Completed***********************************");
    }
    
    @AfterMethod
    public void clostest(ITestResult result ) throws IOException {
-	   if(result.getStatus()==result.FAILURE) {
+	   String screenShotPath = screenshot(driver,result.getName());
+	   if(result.getStatus()==ITestResult.FAILURE) {
+		   System.out.println(result.getStatus());
 		   extenttest.log(LogStatus.FAIL,"Test Failed for "+result.getName());
 		   extenttest.log(LogStatus.FAIL,"Test Failed for "+result.getThrowable());
-		  String screenShotPath = screenshot(driver,result.getName());
+		 // String screenShotPath = screenshot(driver,result.getName());
 		   extenttest.log(LogStatus.FAIL,extenttest.addScreenCapture(screenShotPath));
 	   }
-	   else if(result.getStatus()==result.SKIP){
+	   else if(result.getStatus()==ITestResult.SKIP){
+		   System.out.println(result.getStatus());
 		   extenttest.log(LogStatus.SKIP,"Test Skipped for "+result.getName());
+		  
+		   extenttest.log(LogStatus.FAIL,extenttest.addScreenCapture(screenShotPath));
 		   
 	   }
-	   else if(result.getStatus()==result.SUCCESS) {
-		   
+	   else if(result.getStatus()==ITestResult.SUCCESS) {
+		   System.out.println(result.getStatus());
 		   extenttest.log(LogStatus.PASS,"Test Passed for "+result.getName());
 		   extenttest.log(LogStatus.PASS,"Test Passed for "+result.getThrowable());
-		  String screenShotPath = screenshot(driver,result.getName());
+		  //String screenShotPath = screenshot(driver,result.getName());
 		   extenttest.log(LogStatus.PASS,extenttest.addScreenCapture(screenShotPath));
 		   
 	   }
